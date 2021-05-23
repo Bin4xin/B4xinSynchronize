4f3957fe8ae70e387c00e52c32df73a25f46e180
bin4xin <1904829268@qq.com>
2021-05-23 22:50:55 +0800

    Sun, 23 May 2021 22:50:55 +0800 commit by B4xinSynchronize.

--- bash/functions/banners.sh
+++ bash/functions/banners.sh
@@ -4,7 +4,7 @@ banners_show(){
 arr=(1 2 3 4)
 rand=$[$RANDOM % ${#arr[@]}]
 IFS=$'\n'
-for line in `cat functions/file${arr[$rand]}.txt`
+for line in `cat functions/banner${arr[$rand]}.txt`
 do
     echo $line
 done
